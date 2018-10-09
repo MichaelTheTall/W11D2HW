2919b468ddd7e195652e077b39cce51d56d8e65e
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-09 16:28:11 +0100

    MVP complete

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,40 @@
-public class Library {
+import java.util.ArrayList;
 
+public class Library {
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public Library(int capacity) {
+        this.capacity = capacity;
+        this.stock = new ArrayList<>();
+    }
+
+    public int maxCapacity() {
+        return this.capacity;
+    }
+
+    public int totalOfBooks() {
+        return stock.size();
+    }
+
+    public int capacity() {
+        return this.capacity - totalOfBooks();
+    }
+
+    public void addBook(Book book) {
+        stock.add(book);
+    }
+
+    public String full() {
+        return "The Library is Full!";
+    }
+
+    public String checkSpace(Book book) {
+        if (totalOfBooks() < maxCapacity()) {
+            addBook(book);
+            return "";
+        } else {
+            return full();
+        }
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library;
+    Book book1;
+    Book book2;
+    Book book3;
+
+    @Before
+    public void before() {
+        library = new Library(10);
+        book1 = new Book("The Book", "The Man", "Bookery");
+        book2 = new Book("The Book 2: Electric Boogaloo", "Mr. The Man", "Bookery");
+        book3 = new Book("The Book 3: Books at Sea", "Sir The Man", "Bookery");
+    }
+
+    @Test
+    public void maxCapacity() {
+        assertEquals(10, library.maxCapacity());
+    }
+
+    @Test
+    public void addBook() {
+        library.addBook(book1);
+        assertEquals(9, library.capacity());
+        assertEquals(1, library.totalOfBooks());
+    }
+
+    @Test
+    public void bookTotal() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(3, library.totalOfBooks());
+    }
+
+    @Test
+    public void tooManyBooks() {
+        Library templib = new Library(2);
+        assertEquals("", templib.checkSpace(book1));
+        assertEquals("", templib.checkSpace(book2));
+        assertEquals(2, templib.totalOfBooks());
+        assertEquals("The Library is Full!", templib.checkSpace(book3));
+    }
 }
