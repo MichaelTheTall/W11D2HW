0b1c4ca60d1486a068abe8430d31793ae435d175
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-09 16:33:07 +0100

    start borrower class

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Borrower {
+    private ArrayList<Book> bag;
+
+    public Borrower() {
+        this.bag = new ArrayList<>();
+    }
+
+    public int booksInBag() {
+        return this.bag.size();
+    }
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    Borrower borrower;
+
+    @Before
+    public void before() {
+        borrower = new Borrower();
+    }
+
+    @Test
+    public void inBag() {
+        assertEquals(0, borrower.booksInBag());
+    }
+
 }
