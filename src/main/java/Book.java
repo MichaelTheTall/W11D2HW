2c94563c27cf0f40b1390c7d50ce7141de6f5fa2
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-09 16:10:06 +0100

    Book Class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -8,4 +8,16 @@ public class Book {
         this.author = author;
         this.genre = genre;
     }
+
+    public String title() {
+        return this.title;
+    }
+
+    public String author() {
+        return this.author;
+    }
+
+    public String genre() {
+        return this.genre;
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before() {
+        book = new Book("The Book", "The Man", "Bookery");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("The Book", book.title());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("The Man", book.author());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("Bookery", book.genre());
+    }
+
 }
